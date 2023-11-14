e8124997a630537923b6ba33e086661d666e45f9
Mohamed Sassi <mohamed@msassi.dev>
2023-11-14 00:45:23 -0500

    feat: setup simple page routing with layout and navbar

---
+++ components/NavBar.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/posts/1">Post 1</NuxtLink>
+    <NuxtLink to="/posts/2">Post 2</NuxtLink>
+    <NuxtLink to="/protected">Protected</NuxtLink>
+  </nav>
+</template>
